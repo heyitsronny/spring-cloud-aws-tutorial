5517d1ba0ad2530bfe9ae2aeb3ce8a4daace31fd
Ronald Baumgarten <ronald.baumgarten@idealo.de>
2022-03-01 11:44:08 +0100

    Initial Commit

---
+++ src/main/java/com/bharath/springcloud/repos/CouponRepo.java
@@ -0,0 +1,8 @@
+package com.bharath.springcloud.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bharath.springcloud.model.Coupon;
+
+public interface CouponRepo extends JpaRepository<Coupon, Long> {
+}
