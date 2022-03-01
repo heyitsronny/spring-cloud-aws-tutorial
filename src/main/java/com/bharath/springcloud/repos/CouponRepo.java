02f8ec8d33060b9cec67e938cc64efc2edb2aa1a
Ronald Baumgarten <ronald.baumgarten@idealo.de>
2022-03-01 14:20:43 +0100

    Add GET And POST Endpoints

--- src/main/java/com/bharath/springcloud/repos/CouponRepo.java
+++ src/main/java/com/bharath/springcloud/repos/CouponRepo.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bharath.springcloud.model.Coupon;
 
 public interface CouponRepo extends JpaRepository<Coupon, Long> {
+    Coupon findByCode(String code);
 }
